5908749d16c2b22ccc9949b5fbe1b4ced51bc270
olivercastroteran <olivercastroteran@gmail.com>
2020-04-27 18:13:01 -0400

    form question generator component added and styled

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import List from './components/List';
 import axios from 'axios';
 
 function App() {
   const [flashcards, setFlashcards] = useState(SAMPLE_FALSHCARDS);
+  const [categories, setCategories] = useState([]);
+  const categoryEl = useRef();
+  const amountEl = useRef();
+
+  useEffect(() => {
+    axios.get('https://opentdb.com/api_category.php').then((res) => {
+      setCategories(res.data.trivia_categories);
+    });
+  }, []);
 
   useEffect(() => {
     axios.get('https://opentdb.com/api.php?amount=10').then((res) => {
@@ -31,10 +40,44 @@ function App() {
     return textArea.value;
   }
 
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
+
   return (
-    <div className="container">
-      <List flashcards={flashcards} />
-    </div>
+    <>
+      <form className="header" onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="category">Category</label>
+          <select id="category" ref={categoryEl}>
+            {categories.map((category) => {
+              return (
+                <option value={category.id} key={category.id}>
+                  {category.name}
+                </option>
+              );
+            })}
+          </select>
+        </div>
+        <div className="form-group">
+          <label htmlFor="anoubt">Number of Questions</label>
+          <input
+            type="number"
+            id="amount"
+            min="1"
+            step="1"
+            defaultValue={10}
+            ref={amountEl}
+          />
+        </div>
+        <div className="form-group">
+          <button className="btn">Generate</button>
+        </div>
+      </form>
+      <div className="container">
+        <List flashcards={flashcards} />
+      </div>
+    </>
   );
 }
 
